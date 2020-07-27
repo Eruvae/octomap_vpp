8c12577cf334a4776b6433f10822510bbf180d84
Tobias Zaenker <tzaenker@informatik.uni-bonn.de>
2020-07-28 00:21:38 +0200

    Added header for conversions between octomap and ROS/tf2 datatypes

---
+++ include/octomap_vpp/octomap_transforms.h
@@ -0,0 +1,43 @@
+#ifndef OCTOMAP_PCL_H
+#define OCTOMAP_PCL_H
+
+#include <tf2/convert.h>
+#include <tf2/utils.h>
+#include <octomap/octomap_types.h>
+#include <octomap/Pointcloud.h>
+
+namespace tf2
+{
+
+template <> inline void doTransform(const octomap::point3d& t_in, octomap::point3d& t_out, const geometry_msgs::TransformStamped& transform)
+{
+  tf2::Transform t;
+  fromMsg(transform.transform, t);
+  tf2::Vector3 v_in(t_in.x(), t_in.y(), t_in.z());
+  tf2::Vector3 v_out = t * v_in;
+  t_out = octomap::point3d(v_out.x(), v_out.y(), v_out.z());
+}
+
+} // namespace tf2
+
+namespace octomap_vpp
+{
+
+inline static octomath::Vector3 vectorToOctomath(const geometry_msgs::Vector3 &vec)
+{
+  return octomath::Vector3(vec.x, vec.y, vec.z);
+}
+
+inline static octomath::Quaternion quaternionToOctomath(const geometry_msgs::Quaternion &quat)
+{
+  return octomath::Quaternion(quat.w, quat.x, quat.y, quat.z);
+}
+
+inline static octomath::Pose6D transformToOctomath(const geometry_msgs::Transform &transform)
+{
+  return octomath::Pose6D(vectorToOctomath(transform.translation), quaternionToOctomath(transform.rotation));
+}
+
+} // namespace octomap_vpp
+
+#endif // OCTOMAP_PCL_H
